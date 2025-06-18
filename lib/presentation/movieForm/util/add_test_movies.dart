6b8cb3931d745a298f1c9a638192eee925423a71
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2025-06-18 19:13:26 +0200

    AppIcon

--- lib/presentation/movieForm/util/add_test_movies.dart
+++ lib/presentation/movieForm/util/add_test_movies.dart
@@ -15,7 +15,7 @@ void addTestMovies(
           'Un joven vikingo se hace amigo de un dragón herido y descubre que los dragones no son lo que parecen.',
       trailerUrl:
           'https://www.youtube.com/watch?v=Ns6ZEpQ1xS0&ab_channel=UniversalSpain',
-      category: MovieCategory.action.name,
+      category: MovieCategory.adventure.name,
       type: MovieType.movie,
     ),
     Movie(
@@ -33,13 +33,57 @@ void addTestMovies(
           'Un niño mexicano se embarca en una aventura en la Tierra de los Muertos para descubrir la historia de su familia.',
       trailerUrl:
           'https://www.youtube.com/watch?v=Ga6RYejo6Hk&ab_channel=Pixar',
-      category: MovieCategory.anime.name,
+      category: MovieCategory.fantasy.name,
       type: MovieType.movie,
     ),
+    Movie(
+      title: 'Spider-Man: Un nuevo universo',
+      description:
+          'Miles Morales se convierte en Spider-Man y se une a otros Spider-People de diferentes dimensiones para salvar el multiverso.',
+      trailerUrl:
+          'https://www.youtube.com/watch?v=ii3n7hYQOl4&ab_channel=SonyPicturesEspaña',
+      category: MovieCategory.action.name,
+      type: MovieType.movie,
+    ),
+    Movie(
+      title: 'Avatar',
+      description:
+          'Un ex-marine parapléjico es enviado a la luna Pandora en una misión única, pero se debate entre seguir órdenes y proteger el mundo que siente como suyo.',
+      trailerUrl:
+          'https://www.youtube.com/watch?v=5PSNL1qE6VY&ab_channel=20thCenturyStudiosEspaña',
+      category: MovieCategory.fantasy.name,
+      type: MovieType.movie,
+    ),
+    Movie(
+      title: 'Breaking Bad',
+      description:
+          'Un profesor de química se convierte en fabricante de metanfetamina tras ser diagnosticado con cáncer.',
+      trailerUrl:
+          'https://www.youtube.com/watch?v=HhesaQXLuRY&ab_channel=BreakingBad',
+      category: MovieCategory.drama.name,
+      type: MovieType.series,
+    ),
+    Movie(
+      title: 'The Mandalorian',
+      description:
+          'Un cazarrecompensas viaja por la galaxia protegiendo a un misterioso niño de la misma especie que Yoda.',
+      trailerUrl:
+          'https://www.youtube.com/watch?v=aOC8E8z_ifw&ab_channel=StarWarsEspa%C3%B1a',
+      category: MovieCategory.adventure.name,
+      type: MovieType.series,
+    ),
+    Movie(
+      title: 'The Office',
+      description:
+          'Una comedia sobre la vida diaria de los empleados de una oficina de ventas de papel en Scranton.',
+      trailerUrl:
+          'https://www.youtube.com/watch?v=Vmb1tqYqyII&ab_channel=TheOffice',
+      category: MovieCategory.comedy.name,
+      type: MovieType.series,
+    ),
   ];
-
+  Navigator.of(context).pop();
   for (final movie in movies) {
     await firestoreService.addMovie(movie);
   }
-  Navigator.of(context).pop();
 }
