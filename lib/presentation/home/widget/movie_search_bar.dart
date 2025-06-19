7963fd1e9c62321ef98c33d1e01b759f0f754042
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2025-06-19 20:43:34 +0200

    Added AnimatedContainer to SearchBar

--- lib/presentation/home/widget/movie_search_bar.dart
+++ lib/presentation/home/widget/movie_search_bar.dart
@@ -2,26 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uponorflix/presentation/home/bloc/movie_bloc.dart';
 
-class MovieSearchBar extends StatelessWidget {
+class MovieSearchBar extends StatefulWidget {
   const MovieSearchBar({super.key});
 
   @override
+  State<MovieSearchBar> createState() => _MovieSearchBarState();
+}
+
+class _MovieSearchBarState extends State<MovieSearchBar> {
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
+    final double expandedWidth = MediaQuery.of(context).size.width / 2;
+    const double collapsedWidth = 48;
+
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.ease,
       height: 48,
       padding: const EdgeInsets.only(left: 16),
-      child: SizedBox(
-        width: MediaQuery.of(context).size.width / 2,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+
+        width: _focusNode.hasFocus ? expandedWidth : collapsedWidth,
         child: Builder(
           builder: (context) {
             final searchQuery = context.select<MovieBloc, String>(
               (bloc) => bloc.state.searchQuery,
             );
             return TextField(
+              focusNode: _focusNode,
               onChanged: (value) =>
                   context.read<MovieBloc>().add(ChangeSearchQuery(value)),
               controller: TextEditingController(text: searchQuery)
-                // For correct cursor position
                 ..selection = TextSelection.collapsed(
                   offset: searchQuery.length,
                 ),
@@ -31,9 +60,18 @@ class MovieSearchBar extends StatelessWidget {
                 hintStyle: TextStyle(
                   color: Theme.of(context).colorScheme.primary.withAlpha(150),
                 ),
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: Theme.of(context).colorScheme.primary,
+                prefixIcon: GestureDetector(
+                  onTap: () {
+                    if (_focusNode.hasFocus) {
+                      _focusNode.unfocus();
+                    } else {
+                      FocusScope.of(context).requestFocus(_focusNode);
+                    }
+                  },
+                  child: Icon(
+                    Icons.search,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
                 ),
                 filled: true,
                 fillColor: Theme.of(context).colorScheme.onPrimary,
